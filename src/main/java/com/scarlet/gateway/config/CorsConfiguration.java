e009a5b403e52183c0f376e49114fb412c14d1cc
Lucas Alves <lucas.nascimento.al16@gmail.com>
2023-03-24 18:16:38 -0300

    initial commit

---
+++ src/main/java/com/scarlet/gateway/config/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package com.scarlet.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
